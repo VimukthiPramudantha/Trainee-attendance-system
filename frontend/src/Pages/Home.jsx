d510004da4419839579a080649b816d3763d98f8
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2025-02-12 20:53:11 +0530

    Add HomeComponent with attendance tracking and calendar integration

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import CalendarComponent from '../Components/Calendar'; 
+import HomeComponent from "../Components/HomeComponent";
 
 
 function Home() {
   return (
     <div>
       <Header />
-      <CalendarComponent />
+      <HomeComponent />
       <Footer />
     </div>
   );
