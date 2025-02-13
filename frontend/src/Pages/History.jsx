de21917d2252fa3e3220e98d7404258daec7420d
Vimukthi pramudantha <vimukthipramudanth@gmail.com>
2025-02-13 14:38:30 +0530

    Add HistoryComponent with styling; update Footer and HomeComponent styles for consistency

--- frontend/src/Pages/History.jsx
+++ frontend/src/Pages/History.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../Components/Header";
 import Footer from "../Components/Footer";
+import HistoryComponent from "../Components/HistoryComponent";
 
 function History() {
   return (
     <>
       <Header />
-      <div>History</div>
+      <HistoryComponent/>
       <Footer />
     </>
   );
--- trainee-attendance-backend/controllers/traineeController.js
+++ trainee-attendance-backend/controllers/traineeController.js
@@ -3,11 +3,20 @@ const Trainee = require("../models/Trainee");
 // Add a new trainee
 const addTrainee = async (req, res) => {
   try {
+    console.log("Request Body:", req.body); // Debugging Line
+
     const { traineeID, name, specialization } = req.body;
+    
+    if (!traineeID || !name || !specialization) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
     const newTrainee = new Trainee({ traineeID, name, specialization });
     await newTrainee.save();
+
     res.status(201).json({ message: "Trainee added successfully" });
   } catch (error) {
+    console.error("Error:", error);
     res.status(500).json({ error: "Error adding trainee" });
   }
 };
